9048914aad4221b9db4bade7cdb6ad5bc43abd1d
yaNick811 <167251929+yaNick811@users.noreply.github.com>
2024-05-24 17:07:21 +0300

    Add files via upload

---
+++ nastya.py
@@ -0,0 +1,21 @@
+# def sum(a, b):
+#     return (a + b)
+#
+# a = int(input('Enter 1st number: '))
+# b = int(input('Enter 2nd number: '))
+#
+# print(f'Sum of {a} and {b} is {sum(a, b)}')
+# a = 7
+# b = a
+# print(type(a))
+# print(type(b))
+# print(type(58))
+# print(type(99.9))
+# print(type('abc'))
+prince = 99
+print(prince)
+print(type(5))
+print(type(2.0))
+print(type(5 + 2.0))
+
+
---
+++ nastya2.py
@@ -0,0 +1,53 @@
+# help("keywords")
+import keyword
+keyword.kwlist
+x = 5
+y = x + 12
+print(y)
+
+
+y = 5
+x = 12 - y
+print(x)
+
+
+a = 7
+print(a)
+b = a
+print(b)
+
+
+print(type(7))
+
+isinstance(7, int)
+
+
+two = deux = zwei = 2
+print(two)
+print(deux)
+print(zwei)
+
+
+x = 5
+print(x)
+y = x
+print(y)
+x = 29
+print(x)
+print(y)
+
+
+a = [2, 4, 6]
+b = a
+print(a)
+print(b)
+a[0] = 99
+print(a)
+print(b)
+
+
+prince = 99
+print(prince)
+print(type(5))
+print(type(2.0))
+print(type(5 + 2.0))
